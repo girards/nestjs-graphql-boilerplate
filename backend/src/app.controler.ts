c9094d0c41a381ad1b0d10fe65cdab103e34258d
Nicolas Girardot <nicolas@libeo.io>
2020-04-19 12:49:43 +0200

    fix(CI): change version of chart

--- backend/src/app.controler.ts
+++ backend/src/app.controler.ts
@@ -1,10 +1,16 @@
 import { Get, Controller, Res } from "@nestjs/common";
 import { Response } from "express";
+import { ConfigService } from "@nestjs/config";
 
 @Controller('')
 export class AppController {
+  constructor(private readonly configService: ConfigService) {
+
+  }
+
   @Get()
   healthz(@Res() res: Response): void {
-    res.sendStatus(200);
+    res.json({ branch: this.configService.get<string>('BRANCH') || "localhost" });
+
   }
 }
